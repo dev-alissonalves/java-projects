c8a715e0fd7a34b16bc00f5b6058cb1ba46b285c
Álisson Alves <alissoncc.oliveira@gmail.com>
2021-12-23 22:22:11 -0300

    Java-Projects
    News Basic Projects - Java

---
+++ eclipse/ProjetoCalc/src/code/dio/Calculadora.java
@@ -0,0 +1,42 @@
+package code.dio;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a, b);
+		int subtraçao = subtraçao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+		int divisao = divisao(a, b);
+		
+		System.out.println("Soma:" + soma);
+		System.out.println("Subtração: "+ subtraçao);
+		System.out.println("Multiplicação: " + multiplicacao);
+		System.out.println("Divisão: " + divisao);
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtraçao(int a, int b) {
+		return a - b;
+	}
+	
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+	
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+}
