e38161684f10f5e3a1a7751753cf63df7e4640dd
dev-alissonalves <alissoncc.oliveira@gmail.com>
2022-01-05 11:30:12 -0300

    remove folders

---
+++ eclipse/OlaMundo/src/code/dio/Gato.java
@@ -0,0 +1,70 @@
+package code.dio;
+
+import java.util.Objects;
+
+public class Gato {
+	private String nome;
+	private String raça;
+	private Integer idade;
+	
+	public Gato(String nome, String raça, Integer idade) {
+		super();
+		this.nome = nome;
+		this.raça = raça;
+		this.idade = idade;
+	}
+	
+	public Gato() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaça() {
+		return raça;
+	}
+
+	public void setRaça(String raça) {
+		this.raça = raça;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idade, nome, raça);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gato other = (Gato) obj;
+		return Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome)
+				&& Objects.equals(raça, other.raça);
+	}
+
+	@Override
+	public String toString() {
+		return "Gato [nome=" + nome + ", raça=" + raça + ", idade=" + idade + "]";
+	}
+	
+}
+
+
+
+
---
+++ eclipse/OlaMundo/src/code/dio/Principal.java
@@ -0,0 +1,13 @@
+package code.dio;
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, seja bem vindo ao centro PET.");
+		
+		Gato miau1 = new Gato();
+		Cachorro Auau1 = new Cachorro();
+		
+		System.out.println(miau1);
+		System.out.println(Auau1);	
+	}
+}
